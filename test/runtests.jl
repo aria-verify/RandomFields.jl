bb183de7b26942623d2fa357da1ddbbeb641a886
Matt Graham <matthew.m.graham@gmail.com>
2026-08-14 11:50:06 +0100

    Remove JET tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,11 @@
 using RandomFields
 using Test
 using Aqua
-using JET
+
 
 @testset "RandomFields.jl" begin
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(RandomFields)
     end
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(RandomFields; target_defined_modules = true)
-    end
-    # Write your tests here.
+    @testset "generate! on grid $(summary(grid))" for grid in make_test_grids()
 end
